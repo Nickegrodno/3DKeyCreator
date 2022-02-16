6964b5d2d76ae863ed18474b9cf40828d033c8f4
nickegrodno <nickegrodno@gmail.com>
2022-02-16 13:04:30 +0300

    Change name KeyCreator to Settings

--- KeyCreator/Settings/settings.py
+++ KeyCreator/Settings/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'KeyCreator.urls'
+ROOT_URLCONF = 'Settings.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'KeyCreator.wsgi.application'
+WSGI_APPLICATION = 'Settings.wsgi.application'
 
 
 # Database
