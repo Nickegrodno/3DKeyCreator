88bc31b42d1903e67c775b03acda81869db09c47
nickegrodno <nickegrodno@gmail.com>
2022-03-28 12:44:34 +0300

    Add style to login page

--- keycreator/oauth_app/views.py
+++ keycreator/oauth_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
+#from allauth.account.decorators import verified_email_required
 
 # Create your views here.
 
+#@verified_email_required
 @login_required(login_url="")
 def menu(request) :
     return render(request, "menu.html")
